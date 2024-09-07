8823df6ccbf4c50679a17141e0fc96138833af3e
KubaBorow <jakubrcam@gmail.com>
2024-09-07 08:54:45 +0200

    Add border and single pages

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation='P', unit="mm", format="A4")
+# df - data frame
+df = pd.read_csv('topics.csv')
 
-pdf.add_page()
+for index, row in df.iterrows():
+    pdf.add_page()
 
-pdf.set_font(family="Times", style='B', size=12)
-pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1,)
-
-pdf.set_font(family="Times", size=12)
-pdf.cell(w=0, h=12, txt="Hi There!", align="L", ln=1,)
+    pdf.set_font(family="Times", style='B', size=24)
+    # set color text paleta RGB
+    pdf.set_text_color(0, 128, 255)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    # poczaątek linii x1,y1 koniec linii x2,y2
+    # x1 odleglosc od lewej krawędzi y od lewej gornej krawędzi
+    pdf.line(10, 21, 200, 21)
 
 pdf.output('output.pdf')
